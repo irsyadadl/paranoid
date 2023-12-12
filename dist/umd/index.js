9d50efb8f6e35058fbc6996d3dd7db9630b083fe
Irsyad A. Panjaitan <irsyad@parsinta.com>
2023-12-12 19:33:25 +0700

    fix: fix icon: IconBagBlankFill

--- dist/umd/index.js
+++ dist/umd/index.js
@@ -471,9 +471,6 @@
       fill: "none",
       viewBox: "0 0 24 24"
     }, props), /*#__PURE__*/React__namespace.createElement("path", {
-      fill: "#fff",
-      d: "M0 0h24v24H0z"
-    }), /*#__PURE__*/React__namespace.createElement("path", {
       fill: "currentColor",
       fillRule: "evenodd",
       d: "M8.75 3.5a.25.25 0 0 0-.25.25V6h7V3.75a.25.25 0 0 0-.25-.25zM17 6V3.75A1.75 1.75 0 0 0 15.25 2h-6.5A1.75 1.75 0 0 0 7 3.75V6H3.75A1.75 1.75 0 0 0 2 7.75v11.5c0 .966.784 1.75 1.75 1.75h16.5A1.75 1.75 0 0 0 22 19.25V7.75A1.75 1.75 0 0 0 20.25 6z",
