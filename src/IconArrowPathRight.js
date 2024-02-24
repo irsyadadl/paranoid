e8175d5e433ef3c1df03ec811a2313f21e15baa0
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-02-24 13:22:22 +0700

    :feat

--- src/IconArrowPathRight.js
+++ src/IconArrowPathRight.js
@@ -13,7 +13,7 @@ const SvgIconArrowPathRight = (props) => (
       strokeLinecap="square"
       strokeLinejoin="round"
       strokeWidth={1.5}
-      d="M2.75 14.25v-4.5a1 1 0 0 1 1-1h10V5.34a.5.5 0 0 1 .826-.379l7.294 6.281a1 1 0 0 1 0 1.516l-7.294 6.28a.5.5 0 0 1-.826-.378v-3.41h-10a1 1 0 0 1-1-1Z"
+      d="M2.75 14.25v-4.5a1 1 0 0 1 1-1h10V5.34a.5.5 0 0 1 .826-.378l7.294 6.28a1 1 0 0 1 0 1.516l-7.294 6.28a.5.5 0 0 1-.826-.378v-3.41h-10a1 1 0 0 1-1-1Z"
     />
   </svg>
 );
