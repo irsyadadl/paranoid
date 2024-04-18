1bf1a8887503d7001e6504981ab79862252ef9a1
Irsyad A. Panjaitan <irsyad@parsinta.com>
2024-04-19 02:38:24 +0700

    :fix

---
+++ src/IconChevronDoubleLeft.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgIconChevronDoubleLeft = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="square"
+      strokeWidth={2}
+      d="m10 16-4-4 4-4m7 8-4-4 4-4"
+    />
+  </svg>
+);
+export default SvgIconChevronDoubleLeft;
---
+++ src/IconMinus.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SvgIconMinus = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeWidth={1.5}
+      d="M3.75 12h16.5"
+    />
+  </svg>
+);
+export default SvgIconMinus;
